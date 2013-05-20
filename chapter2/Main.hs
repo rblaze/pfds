51e20d415def46ad0cfb601f037c285393c325da
Andrey Sverdlichenko <blaze@ruddy.ru>
2013-05-20 17:08:09 +0400

    Optimized member function from exercise 2.2

--- chapter2/Main.hs
+++ chapter2/Main.hs
@@ -9,6 +9,7 @@ import Data.List (tails)
 
 import PFDS21
 import PFDS2Set
+import PFDS22
 
 tests :: [Test]
 tests = [
@@ -17,9 +18,14 @@ tests = [
         testCase "book sample" testSample21
       ],
     testGroup "basic set" [
-        testProperty "all members found" prop_setMembersFound,
-        testProperty "some members found" prop_setSomeMembersFound,
-        testProperty "no members in empty set" prop_setEmpty
+        testProperty "all members found" (prop_setMembersFound member),
+        testProperty "some members found" (prop_setSomeMembersFound member),
+        testProperty "no members in empty set" (prop_setEmpty member)
+      ],
+    testGroup "optimized member" [
+        testProperty "all members found" (prop_setMembersFound member22),
+        testProperty "some members found" (prop_setSomeMembersFound member22),
+        testProperty "no members in empty set" (prop_setEmpty member22)
       ]
   ]
 
@@ -29,16 +35,16 @@ main = defaultMain tests
 prop_tails :: [Int] -> Bool
 prop_tails xs = suffixes xs == tails xs
 
-prop_setMembersFound :: [Int] -> Bool
-prop_setMembersFound xs = all (`member` set) xs
-    where set = foldr insert Empty xs
-
-prop_setSomeMembersFound :: [Int] -> Bool
-prop_setSomeMembersFound xs = all (\x -> member (x + 1) set == elem (x + 1) xs) xs
-    where set = foldr insert Empty xs
-
-prop_setEmpty :: [Int] -> Bool
-prop_setEmpty = all (\x -> not (member x Empty))
-
 testSample21 :: Assertion
 testSample21 = suffixes ([1,2,3,4] :: [Int]) @?= [[1,2,3,4],[2,3,4],[3,4],[4],[]]
+
+prop_setMembersFound :: (Int -> Tree Int -> Bool) -> [Int] -> Bool
+prop_setMembersFound check xs = all (`check` set) xs
+    where set = foldr insert Empty xs
+
+prop_setSomeMembersFound :: (Int -> Tree Int -> Bool) -> [Int] -> Bool
+prop_setSomeMembersFound check xs = all (\x -> check (x + 1) set == elem (x + 1) xs) xs
+    where set = foldr insert Empty xs
+
+prop_setEmpty :: (Int -> Tree Int -> Bool) -> [Int] -> Bool
+prop_setEmpty check = all (\x -> not (check x Empty))
--- chapter2/PFDS2Set.hs
+++ chapter2/PFDS2Set.hs
@@ -6,13 +6,13 @@ data Tree a = Empty
 member :: Ord a => a -> Tree a -> Bool
 member _ Empty = False
 member val (Tree left key right)
-    | val == key    = True
     | val < key     = member val left
-    | otherwise     = member val right
+    | val > key     = member val right
+    | otherwise     = True
 
 insert :: Ord a => a -> Tree a -> Tree a
 insert val Empty = Tree Empty val Empty
 insert val node@(Tree left key right)
-    | val == key    = node
     | val < key     = Tree (insert val left) key right
-    | otherwise     = Tree left key (insert val right)
+    | val > key     = Tree left key (insert val right)
+    | otherwise     = node
