f0737ef2cf8fda53bc805f33aae7330374308ed1
Andrey Sverdlichenko <blaze@ruddy.ru>
2013-05-21 12:15:20 +0400

    monadic style

--- chapter2/PFDS23.hs
+++ chapter2/PFDS23.hs
@@ -8,11 +8,11 @@ insert23 :: Ord a => a -> Tree a -> Tree a
 insert23 val tree = fromMaybe tree (go tree)
     where
     go Empty = Just (Tree Empty val Empty)
-    go (Tree left key right) = case compare val key of
-                                    EQ -> Nothing
-                                    LT -> case go left of
-                                            Nothing -> Nothing
-                                            Just t -> Just (Tree t key right)
-                                    GT -> case go right of
-                                            Nothing -> Nothing
-                                            Just t -> Just (Tree left key t)
+    go (Tree left key right)
+        | val < key = do
+                        t <- go left
+                        return (Tree t key right)
+        | val > key = do
+                        t <- go right
+                        return (Tree left key t)
+        | otherwise = Nothing
