f8f6ba1a8af47a9b13e2dfeadc53ae8f5d9400a4
Andrey Sverdlichenko <blaze@ruddy.ru>
2013-05-23 12:29:03 +0400

    Optimized version of 2.5b

--- chapter2/PFDS25.hs
+++ chapter2/PFDS25.hs
@@ -9,12 +9,10 @@ mktree25a el depth = go depth Empty
     go n subtree = go (n - 1) (Tree subtree el subtree)
 
 mktree25b :: a -> Int -> Tree a
-mktree25b el cnt
-    | cnt == 0  = Empty
-    | odd cnt   = let half = cnt `div` 2
-                      subtree = mktree25b el half
-                   in Tree subtree el subtree
-    | otherwise = let half = cnt `div` 2
-                      left = mktree25b el half
-                      right = mktree25b el (half - 1)
-                   in Tree left el right
+mktree25b el cnt = fst $ create2 cnt
+    where
+    create2 0 = (Empty, Tree Empty el Empty)
+    create2 n | odd n = let (sub0, sub1) = create2 (n `div` 2)
+                         in (Tree sub0 el sub0, Tree sub1 el sub0)
+    create2 n = let (sub0, sub1) = create2 ((n `div` 2) - 1)
+                 in (Tree sub0 el sub1, Tree sub1 el sub1)
