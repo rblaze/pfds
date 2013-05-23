62a6073c65d43a2591874d90faa641a1ca8d32a6
Andrey Sverdlichenko <blaze@ruddy.ru>
2013-05-23 18:20:02 +0400

    Exercise 3.3

--- chapter3/Main.hs
+++ chapter3/Main.hs
@@ -6,30 +6,32 @@ import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Data.List(sort)
 
 import BaseHeap
+import PFDS33
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: [Test]
 tests = [
-    heapTests "basic heap" insert
+    heapTests "basic heap" (foldr insert Empty),
+    heapTests "fromList" fromList
   ]
 
-heapTests :: String -> (Int -> Heap Int -> Heap Int) -> Test
+heapTests :: String -> ([Int] -> Heap Int) -> Test
 heapTests name insertFunc = testGroup name [
     testProperty "heap contains all elements" (prop_allElemsPresent insertFunc),
     testProperty "heap elements sorted" (prop_heapElementsSorted insertFunc)
   ]
 
-prop_allElemsPresent :: (Int -> Heap Int -> Heap Int) -> [Int] -> Bool
-prop_allElemsPresent ins xs = sort xs == sort (toList heap)
+prop_allElemsPresent :: ([Int] -> Heap Int) -> [Int] -> Bool
+prop_allElemsPresent mkheap xs = sort xs == sort (toList heap)
     where
-    heap = foldr ins Empty xs
+    heap = mkheap xs
 
-prop_heapElementsSorted :: (Int -> Heap Int -> Heap Int) -> [Int] -> Bool
-prop_heapElementsSorted ins xs = sort xs == unroll heap
+prop_heapElementsSorted :: ([Int] -> Heap Int) -> [Int] -> Bool
+prop_heapElementsSorted mkheap xs = sort xs == unroll heap
     where
-    heap = foldr ins Empty xs
+    heap = mkheap xs
     unroll h = case findMin h of
                 Nothing -> []
                 Just v  -> v : unroll (deleteMin h)
---
+++ chapter3/PFDS33.hs
@@ -0,0 +1,15 @@
+module PFDS33 where
+
+import BaseHeap
+
+fromList :: Ord a => [a] -> Heap a
+fromList elems = go $ map (\v -> Heap 1 v Empty Empty) elems
+    where
+    step :: Ord a => [Heap a] -> [Heap a]
+    step (h1:h2:hs) = (merge h1 h2) : step hs
+    step h = h
+
+    go :: Ord a => [Heap a] -> Heap a
+    go [] = Empty
+    go [s] = s
+    go hs = go $ step hs
