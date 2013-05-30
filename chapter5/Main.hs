7ae46d9da8490c1a384df2d8abaa218e61e9167b
Andrey Sverdlichenko <blaze@ruddy.ru>
2013-05-30 20:16:01 +0400

    Keeping invariants from book, better tests

--- chapter5/Main.hs
+++ chapter5/Main.hs
@@ -4,7 +4,6 @@ module Main where
 
 import Test.Framework (defaultMain, testGroup, Test)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Test.QuickCheck(Arbitrary)
 import Data.List(foldl')
 
 import qualified Queue as Q
@@ -14,12 +13,26 @@ main = defaultMain tests
 
 tests :: [Test]
 tests = [
-    testProperty "queue keeps all elements" prop_inout
+    testProperty "queue keeps all elements" prop_inout,
+    testProperty "queue keeps all elements in complex case" prop_inout3
   ]
 
 prop_inout :: [Int] -> Bool
 prop_inout xs = xs == popAll queue
     where
-    queue = foldl' Q.push Q.empty xs
+    queue = foldl' Q.snoc Q.empty xs
     popAll q | Q.null q = []
-    popAll q = Q.head q : popAll (Q.pop q)
+    popAll q = Q.head q : popAll (Q.tail q)
+
+prop_inout3 :: [Int] -> [Int] -> [Int] -> Bool
+prop_inout3 xs ys zs = concat [xs, ys, zs] == popped && Q.null queue
+    where
+    queuex = foldl' Q.snoc Q.empty xs
+    queuexy = foldl' Q.snoc queuex ys
+    (queuey, poppedx) = popN (length xs) queuexy
+    queueyz = foldl' Q.snoc queuey zs
+    (queue, poppedyz) = popN (length ys + length zs) queueyz
+    popped = poppedx ++ poppedyz
+    
+    popN 0 q = (q, [])
+    popN n q = let (tq, l) = popN (n - 1) (Q.tail q) in (tq, Q.head q : l)
--- chapter5/Queue.hs
+++ chapter5/Queue.hs
@@ -1,5 +1,6 @@
 module Queue where
 
+import Prelude hiding(tail)
 import Data.Foldable
 
 data Queue a = Queue [a] [a]
@@ -8,7 +9,8 @@ instance Functor Queue where
     fmap f (Queue qin qout) = Queue (fmap f qin) (fmap f qout)
 
 instance Foldable Queue where
-    foldr f z (Queue qin qout) = foldl' (flip f) (Prelude.foldr f z qin) qout
+    foldr f z (Queue qin qout) = let tmp = Prelude.foldr f z qin
+                                  in foldl' (flip f) tmp qout
 
 empty :: Queue a
 empty = Queue [] []
@@ -17,18 +19,21 @@ null :: Queue a -> Bool
 null (Queue [] []) = True
 null _ = False
 
-push :: Queue a -> a -> Queue a 
-push (Queue qin qout) v = Queue (v:qin) qout
+snoc :: Queue a -> a -> Queue a 
+snoc (Queue [] []) v = Queue [] [v]
+snoc (Queue _ []) _ = error "invariant failed"
+snoc (Queue qin qout) v = Queue (v:qin) qout
 
-pop :: Queue a -> Queue a
-pop (Queue [] []) = error "Queue underrun (pop)" 
-pop (Queue qin (_:xs)) = Queue qin xs
-pop (Queue qin []) = pop (Queue [] (reverse qin))
+tail :: Queue a -> Queue a
+tail (Queue [] []) = error "Queue underrun (pop)" 
+tail (Queue qin (_:[])) = Queue [] (reverse qin)
+tail (Queue qin (_:xs)) = Queue qin xs
+tail (Queue _ _) = error "tail invariant failed"
 
 head :: Queue a -> a
 head (Queue [] []) = error "Queue underrun (head)"
 head (Queue _ (x:_)) = x
-head (Queue qin []) = Prelude.last qin
+head (Queue _ _) = error "head invariant failed"
 
 last :: Queue a -> a
 last (Queue [] []) = error "Queue underrun (last)"
